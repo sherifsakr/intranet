4cd1109abd3f98a8035b83dda2eda062d60c4b53
sherif ibrahim kamel sakr <elsherif_sakr@hotmail.com>
2018-04-05 01:24:43 +0300

    u

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var app = angular.module("internalApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "pages/home.html"
+        templateUrl : "pages/home.html",
+        controller : "mainCtrl"
     })
     .when("/boardManag", {
         templateUrl : "pages/boardManag.html"
@@ -11,12 +12,61 @@ app.config(function($routeProvider) {
      .when("/orgChart", {
         templateUrl : "pages/orgChart.html"
     })
-    .when("/green", {
-        templateUrl : "green.htm"
+    .when("/missionAndVision", {
+        templateUrl : "pages/missionAndVision.html",
+        controller : "missionAndVisionCtrl"
     })
-    .when("/blue", {
-        templateUrl : "blue.htm"
-    });
+     .when("/missions", {
+        templateUrl : "pages/missions.html",
+    
+    })
 });
 
+
+app.controller("mainCtrl", function ($scope) {
+	
+	$scope.$on('$viewContentLoaded', function(event) {
+		//Your code goes here.
+	    $('#js-news').ticker();
+		});
+	
+	 $(document).ready(function(){
+
+		    var clickEvent = false;
+		    $('#demo').carousel({
+		    interval:   4000
+		    }).on('click', '.list-group li', function() {
+		      clickEvent = true;
+		      $('.list-group li').removeClass('active');
+		      $(this).addClass('active');
+		    }).on('slid.bs.carousel', function(e) {
+		    if(!clickEvent) {
+		      var count = $('.list-group').children().length -1;
+		      var current = $('.list-group li.active');
+		      current.removeClass('active').next().addClass('active');
+		      var id = parseInt(current.data('slide-to'));
+		      if(count == id) {
+		        $('.list-group li').first().addClass('active');
+		      }
+		    }
+		    clickEvent = false;
+		    });
+		    })
+		    
+	 $(window).load(function() {
+		    var boxheight = $('#demo .carousel-inner').innerHeight();
+		    var itemlength = $('#demo .item').length;
+		    var triggerheight = Math.round(boxheight/itemlength+1);
+		    $('#demo .list-group-item').outerHeight(triggerheight);
+		    });
+	 
+});
+
+
+app.controller('orgChartctrl', function($scope) {
+
+});
+app.controller('missionAndVisionCtrl', function($scope) {
+
+});
 
